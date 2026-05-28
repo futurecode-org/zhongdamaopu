d7dcb17f548d37eb7faaea5b9e24a437da35f74b
不死鸟Anka <47747022+A-n-k-a@users.noreply.github.com>
2026-05-28 15:19:15 +0800

    更新图标主色调；更新关于页面上的logo

--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -5,7 +5,8 @@ import { showTab } from "../../utils/page";
 
 const share_text = text_cfg.app_name + ' - ' + text_cfg.info.share_tip;
 
-const logo_img = "/pages/public/images/app_logo.png";
+// const logo_img = "/pages/public/images/app_logo.png";
+const logo_img = "/pages/public/images/totoro_transparent_background.png";
 
 import { signCosUrl } from "../../utils/common";
 
